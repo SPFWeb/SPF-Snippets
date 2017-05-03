810d1a30bcfe465a9042a6aabdf19716afa1e5b6
SPF Websites & Multimedia <spfschools@gmail.com>
2017-05-04 10:33:44 +1200

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -48,3 +48,23 @@ function property_info_save( $post_id ) {
 		update_post_meta( $post_id, 'property_info_price_', esc_attr( $_POST['property_info_price_'] ) );
 }
 add_action( 'save_post', 'property_info_save' );
+
+// Add Shortcode
+// [info people="2" price="$250"]
+function custom_shortcode( $atts ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'people' => '2',
+			'price' => '$0',
+		),
+		$atts,
+		'info'
+	);
+
+	return '<div style="float: right;border-left: 1px solid #c6c6c6;border-left-width: 2px;padding: 5px 10px 0px 40px;text-align:center;"><h2>' . $atts['price'] . ' NZD</h2> <i>Per Night</i><br /><i class="wk-icon-users" style="font-size: 15px;padding: 0px;margin-right: 10px;"></i> x' .$atts['people']. ' People</div>';
+
+
+}
+add_shortcode( 'info', 'custom_shortcode' );
